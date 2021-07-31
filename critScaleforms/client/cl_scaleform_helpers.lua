8272431dadcb491d6f1b6e72796f86fa6dfa1b52
PhilippRedel <philipp-redel@web.de>
2021-07-31 10:33:53 +0200

    Reworked all scaleforms to use global functions (except credits which need more research). Fixed the MissionQuit bug #4.

---
+++ critScaleforms/client/cl_scaleform_helpers.lua
@@ -0,0 +1,38 @@
+Scaleform = {}
+
+function Scaleform.Request(scaleform)
+    local scaleform_handle = RequestScaleformMovie(scaleform)
+    while not HasScaleformMovieLoaded(scaleform_handle) do
+        Citizen.Wait(0)
+    end
+    return scaleform_handle
+end
+
+function Scaleform.CallFunction(scaleform, returndata, the_function, ...)
+    BeginScaleformMovieMethod(scaleform, the_function)
+    local args = {...}
+
+    if args ~= nil then
+        for i = 1,#args do
+            local arg_type = type(args[i])
+
+            if arg_type == "boolean" then
+                ScaleformMovieMethodAddParamBool(args[i])
+            elseif arg_type == "number" then
+                if not string.find(args[i], '%.') then
+                    ScaleformMovieMethodAddParamInt(args[i])
+                else
+                    ScaleformMovieMethodAddParamFloat(args[i])
+                end
+            elseif arg_type == "string" then
+                ScaleformMovieMethodAddParamTextureNameString(args[i])
+            end
+        end
+
+        if not returndata then
+            EndScaleformMovieMethod()
+        else
+            EndScaleformMovieMethodReturnValue()
+        end
+    end
+end
--- critScaleforms/fxmanifest.lua
+++ critScaleforms/fxmanifest.lua
@@ -5,9 +5,10 @@ author 'CritteR / CritteRo'
 description 'A coletion of scaleforms that can be integrated easily in your scripts.'
 
 client_scripts {
+    'client/cl_scaleform_helpers.lua',
     'client/cl_scaleform_functions.lua',
     'client/cl_main.lua',
-    'client/cl_examples.lua',
+    'client/cl_examples.lua'
 }
 
 server_scripts {
